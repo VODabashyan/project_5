fc1d8fa67b2b22c198cccc419aa6e88d9745d0d8
Vahe Odabashyan <vaheodabashyan@gmail.com>
2022-10-01 18:30:52 +0400

    added the DB and created the table

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const sqlite = require("sqlite3").verbose();
 const app = express();
+
+const data = new sqlite.Database("data.db");
+data.run("create table if not exists contacts (id integer primary key autoincrement, fullName text not null, address text not null, phoneNumber text not null, email text not null)");
 
 app.get("/test", (req, res) => {
     res.status(200).json({success: true})
